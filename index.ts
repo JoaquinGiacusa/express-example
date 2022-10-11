7efc49de6142ff23ff115e1380cac570733eb60d
JoaquinGiacusa <joaquingiacusa.dev@gmail.com>
2022-10-11 19:24:35 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hola mundo" });
+});
+
+app.listen(3000, () => {
+  return console.log(`Server running on 3000`);
+});
