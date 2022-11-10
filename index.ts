c9ca76c704c3ff2af4bfc5bbfea97c57041614e8
JoaquinGiacusa <joaquingiacusa.dev@gmail.com>
2022-11-10 17:02:26 -0300

    fix search

--- index.ts
+++ index.ts
@@ -5,16 +5,48 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const products: { id: number; name: string; marca: string }[] = [
+type Product = { id: number; name: string; brand: string };
+
+const products: Product[] = [
   {
     name: "Teclado",
-    marca: "Logitech",
+    brand: "Logitech",
     id: 12356234,
   },
   {
     name: "Placa",
-    marca: "AMD",
+    brand: "AMD",
     id: 42345612,
+  },
+  {
+    name: "Placa",
+    brand: "AMD",
+    id: 4235232,
+  },
+  {
+    name: "Placa",
+    brand: "AMD",
+    id: 4234612,
+  },
+  {
+    name: "Placa",
+    brand: "AMD",
+    id: 474612,
+  },
+  {
+    name: "Placa",
+    brand: "AMD",
+    id: 4234123,
+  },
+  {
+    name: "Placa",
+    brand: "AMD",
+    id: 425312,
+  },
+  {
+    name: "Placa",
+    brand: "AMD",
+    id: 4234212,
   },
 ];
 
@@ -22,7 +54,42 @@ const products: { id: number; name: string; marca: string }[] = [
  * Get all products
  */
 app.get("/product", (req, res) => {
-  return res.status(200).json(products);
+  const query = req.query as {
+    // name?: string;
+    // brand?: string;
+    search: string;
+    limit?: string;
+    page?: string;
+  };
+
+  let currentProducts: Product[] = products;
+  let totalPage = 0;
+  let limit = query?.limit ? Number(query?.limit) : 5;
+
+  if (query.search) {
+    currentProducts = currentProducts.filter(
+      (product) =>
+        product.name.toLowerCase().includes(query.search.toLowerCase()) ||
+        product.brand.toLowerCase().includes(query.search.toLowerCase())
+    );
+  }
+
+  // if (query.brand) {
+  //   currentProducts = currentProducts.filter((product) =>
+  //     product.brand.toLowerCase().includes(query.brand.toLowerCase())
+  //   );
+  //}
+
+  if (query.page) {
+    const page = Number(query.page);
+    // console.log(currentProducts.length);
+
+    totalPage = Math.ceil(currentProducts.length / limit);
+    const result = currentProducts.slice((page - 1) * limit, limit * page);
+    return res.status(200).json({ result, totalPage });
+  }
+
+  return res.status(200).json({ result: currentProducts });
 });
 
 /**
@@ -41,12 +108,12 @@ app.get("/product/:id", (req, res) => {
  * Create product
  */
 app.post("/product", (req, res) => {
-  const { name, marca } = req.body;
-  if (!name || !marca)
+  const { name, brand } = req.body;
+  if (!name || !brand)
     return res
       .status(400)
-      .json({ message: "fields 'name' and 'marca' is requiered" });
-  products.push({ id: new Date().getTime(), name, marca });
+      .json({ message: "fields 'name' and 'brand' is requiered" });
+  products.push({ id: new Date().getTime(), name, brand });
   return res.status(200).json(...products.slice(-1));
 });
 
@@ -54,14 +121,15 @@ app.post("/product", (req, res) => {
  * Edit product by id
  */
 app.put("/product/:id", (req, res) => {
-  const { name, marca } = req.body;
+  const { name, brand } = req.body;
   const id = Number(req.params.id);
   const index = products.findIndex((product) => product.id === id);
+  console.log(name, brand);
 
   if (index === -1)
     return res.status(400).json({ message: "no se encontro producto" });
-  if (marca) {
-    products[index].marca = marca;
+  if (brand) {
+    products[index].brand = brand;
   }
   if (name) {
     products[index].name = name;
@@ -83,58 +151,5 @@ app.delete("/product/:id", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("Corriendo");
-});
-
-const getOffsetAndLimitFromReq = (
-  limit,
-  offset,
-  maxLimit = 10,
-  maxOffset = 100
-) => {
-  // const queryLimit = Number(limit);
-  // const queryOffset = Number(offset) || 0;
-
-  const finalLimit = limit ? (limit < maxLimit ? limit : maxLimit) : maxLimit;
-
-  const finalOffset = offset < maxOffset ? offset : 0;
-
-  return { finalLimit, finalOffset };
-};
-
-/**
- * Search Products by query
- */
-app.get("/search", (req, res) => {
-  const { q, limit, offset } = req.query;
-  const queryLimit = Number(limit);
-  const queryOffset = Number(offset);
-
-  const { finalLimit, finalOffset } = getOffsetAndLimitFromReq(
-    queryLimit,
-    queryOffset
-  );
-
-  const search = products.filter((p) => {
-    if (
-      p.name.toLocaleLowerCase().includes(q as string) ||
-      p.marca.toLocaleLowerCase().includes(q as string)
-    )
-      return p;
-  });
-
-  if (search.length > 0) {
-    if (search.length > finalOffset) {
-      const filtered = search
-        .slice(finalOffset, search.length)
-        .slice(0, finalLimit);
-
-      return res.status(200).json({ serachResult: filtered });
-    } else {
-      return res.status(200).json({ serachResult: search });
-    }
-  } else
-    return res
-      .status(400)
-      .json({ message: "No hubo coincidencias con la busqueda" });
+  console.log("Corriendo en el puerto 3000");
 });
